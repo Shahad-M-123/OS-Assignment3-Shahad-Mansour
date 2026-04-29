b91b9416ed9a03bbe88d97423ad9161af2e36ca3
Shahad-M-123 <445052028@std.psau.edu.sa>
2026-04-29 19:52:42 +0300

    co2

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -40,24 +40,24 @@ class SharedResources {
     public static int completedProcessCount = 0;  
     public static long totalWaitingTime = 0;      
     public static List<String> executionLog = new ArrayList<>();  
-    
-    // TODO #1: Add a ReentrantLock(s) here to protect critical sections
-    // Example: public static final ReentrantLock lock = new ReentrantLock();
-    
-    // TODO #2: Add a Semaphore to limit concurrent process execution
-    // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
-    
     // Method to increment context switch counter
     public static void incrementContextSwitch() {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: Multiple threads might read and write simultaneously!
-        contextSwitchCount++;
+        contextSwitchLock.lock();
+        try {
+            contextSwitchCount++;
+        } finally {
+            contextSwitchLock.unlock();
+        }
     }
     
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
-        // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+         completedProcessLock.lock();
+        try {
+            completedProcessCount++;
+        } finally {
+            completedProcessLock.unlock();
+        }
     }
     
     // Method to add waiting time
